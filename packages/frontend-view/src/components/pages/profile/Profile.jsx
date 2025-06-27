c5113d7fb11732b549a1ffc32d1772429bff6bc8
srj-17 <saugatrijal17@gmail.com>
2025-06-27 07:42:52 +0545

    refactor: Separate fetching logic from the component
    I have a hunch it might make my fetching experience
    easier later

--- packages/frontend-view/src/components/pages/profile/Profile.jsx
+++ packages/frontend-view/src/components/pages/profile/Profile.jsx
@@ -1,58 +1,10 @@
-import { useState } from "react";
-import { useEffect } from "react";
 import { useParams } from "react-router-dom";
-
-function useUser(userId) {
-    const [loggedIn, setLoggedIn] = useState(true);
-    const [user, setUser] = useState(null);
-    const [error, setError] = useState(false);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        async function getUser() {
-            try {
-                const jwtToken = localStorage.getItem("token");
-
-                if (!jwtToken) {
-                    setLoggedIn(false);
-                }
-
-                const userURL = `http://localhost:3000/users/${userId}`;
-                const response = await fetch(userURL, {
-                    cors: true,
-                    headers: {
-                        Authorization: `Bearer ${jwtToken}`,
-                    },
-                });
-                const jsonData = await response.json();
-
-                // 403 = forbidden
-                if (response.status >= 400) {
-                    if (response.status === 403) {
-                        localStorage.removeItem("token");
-                        setLoggedIn(false);
-                    }
-                    console.error(jsonData.msg);
-                    setError(true);
-                }
-
-                setUser(jsonData);
-            } catch (e) {
-                console.error(e);
-                setError(true);
-            }
-            setLoading(false);
-        }
-
-        getUser();
-    }, [userId]);
-
-    return { loggedIn, user, error, loading };
-}
+import { useFetch } from "../../../utils/fetch";
 
 export default function Profile() {
     const { userId } = useParams();
-    const { loggedIn, user, error, loading } = useUser(userId);
+    const url = `http://localhost:3000/users/${userId}`;
+    const { loggedIn, data: user, error, loading } = useFetch(url);
 
     if (!loggedIn) {
         return (
