71ebbfd1f89368984b0b124e435d3c1fce6987d9
srj-17 <saugatrijal17@gmail.com>
2025-07-06 15:11:43 +0545

    fix(frontend-edit): Fix published / created Date not showing up
    publishedDate -> publishedAt

--- packages/frontend-edit/src/components/BlogCard.jsx
+++ packages/frontend-edit/src/components/BlogCard.jsx
@@ -1,13 +1,16 @@
 import styles from "./BlogCard.module.css";
 import Button from "#components/Button";
 import capitalize from "#utils/capitalize";
+import dateStringToReadableDate from "#utils/dateStringToReadableDate";
 
 export default function BlogCard({ blog }) {
     return (
         <div className={styles.blogCard}>
             <div className={styles.blogInfo}>
                 <div className={styles.blogDate}>
-                    {blog.published ? blog.publishedDate : blog.createdDate}
+                    {blog.published
+                        ? `Published At: ${dateStringToReadableDate(blog.publishedAt)}`
+                        : `Created At: ${dateStringToReadableDate(blog.createdAt)}`}
                 </div>
                 <div className={styles.blogTitle}>{capitalize(blog.title)}</div>
             </div>
