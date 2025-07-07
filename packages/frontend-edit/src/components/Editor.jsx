cd3f0b55a22c79109536fee614f6855040050fa0
srj-17 <saugatrijal17@gmail.com>
2025-07-07 18:44:56 +0545

    feat(frontend-edit): Add feature to edit any blog

--- packages/frontend-edit/src/components/Editor.jsx
+++ packages/frontend-edit/src/components/Editor.jsx
@@ -4,10 +4,14 @@ import Quill from "quill";
 // Editor is an uncontrolled React component
 export default function Editor({
     // this ref is the quillRef that we passed from parent
+    // this points to the editorContainer as well, but it's
+    // here so that we can pass the reference to editor to the
+    // parent.
     ref,
     defaultValue,
     setBlogBody,
 }) {
+    // points to the editor container
     const containerRef = useRef(null);
 
     useEffect(() => {
--- packages/frontend-edit/src/pages/editBlog/EditBlogPage.jsx
+++ packages/frontend-edit/src/pages/editBlog/EditBlogPage.jsx
@@ -1,11 +1,125 @@
-import { useParams } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 import styles from "./EditBlogPage.module.css";
+import HomeButton from "#components/HomeButton";
+import Quill from "quill";
+import { useState, useEffect, useRef } from "react";
+import Editor from "#components/Editor";
+import Button from "#components/Button";
+import Loading from "#components/Loading";
+import ErrorPage from "../error/ErrorPage";
+import UnauthorizedPage from "../unauthorizedPage/UnauthorizedPage";
+import { useFetch } from "#utils/fetch";
 
 export default function EditBlogPage() {
     const { blogId } = useParams();
+    const blogUrl = `http://localhost:3000/posts/${blogId}`;
+    const [blogTitle, setBlogTitle] = useState("");
+    const [blogBody, setBlogBody] = useState(null);
+    const [blogSubmitted, setBlogSubmitted] = useState(false);
+    const { loggedIn, data: blogData, error, loading } = useFetch(blogUrl);
+
+    // Use a ref to access the quill instance directly
+    const quillRef = useRef();
+
+    useEffect(() => {
+        if (!error && !loading) {
+            setBlogTitle(blogData.title);
+
+            // we'll have the reference to editor once the
+            // child is mounted and updates the current
+            setBlogBody(quillRef.current.getContents());
+        }
+    }, [loading]);
+
+    async function handlePublishBlog(publishStatus = false) {
+        const publishUrl = `http://localhost:3000/posts/${blogId}`;
+        const bodyContent = {
+            title: blogTitle,
+            content: JSON.stringify(blogBody),
+        };
+
+        if (publishStatus === true) {
+            bodyContent.published = true;
+        }
+
+        const response = await fetch(publishUrl, {
+            mode: "cors",
+            method: "PUT",
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(bodyContent),
+        });
+
+        // TODO: handle error cases
+        if (response.ok) {
+            setBlogTitle("");
+            setBlogBody(null);
+            setBlogSubmitted(true);
+        }
+    }
+
+    if (loading) return <Loading />;
+
+    if (!loggedIn) return <UnauthorizedPage />;
+
+    if (error) return <ErrorPage statusCode={500} message={blogData.message} />;
+
     return (
-        <div className={styles.editBlogPage}>
-            This is where you edit blog {blogId}
-        </div>
+        <>
+            {blogSubmitted ? (
+                <Navigate to="/" />
+            ) : (
+                <div className={styles.editBlogPage}>
+                    <HomeButton />
+                    <div className={styles.editBlogHeader}>Edit Blog</div>
+                    <div className={styles.blogEditorContainer}>
+                        <div className={styles.titleContainer}>
+                            <label
+                                htmlFor="title"
+                                className={styles.titleLabel}
+                            >
+                                Title
+                            </label>
+                            <input
+                                className={styles.titleInput}
+                                placeholder="Never gonna give you up"
+                                id="title"
+                                type="text"
+                                name="title"
+                                required
+                                value={blogTitle}
+                                onChange={(e) => {
+                                    setBlogTitle(e.target.value);
+                                }}
+                            />
+                        </div>
+                        <Editor
+                            ref={quillRef}
+                            defaultValue={JSON.parse(blogData.content)}
+                            setBlogBody={setBlogBody}
+                        />
+                    </div>
+                    <div className={styles.createBlogActions}>
+                        <Button
+                            onClick={() => {
+                                handlePublishBlog(true);
+                            }}
+                        >
+                            Publish Blog
+                        </Button>
+                        <Button
+                            onClick={() => {
+                                handlePublishBlog(false);
+                            }}
+                        >
+                            Save As Draft
+                        </Button>
+                        <Button>Discard Blog</Button>
+                    </div>
+                </div>
+            )}
+        </>
     );
 }
